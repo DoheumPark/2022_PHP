31087a5f21c37da41800271a85dbf9545a24a3fe
sbsteacher <pirbak@gmail.com>
2022-04-11 13:15:31 +0900

    220411_test

--- ch05/foreach2.php
+++ ch05/foreach2.php
@@ -1,13 +1,10 @@
+<table>
 <?php
-    /*
-        
-    */
     $array = array(
         "Hong" => 182.2,
         "Hwang" => 180.4,
         "Kim" => 176.3,
         "Park" => 174.1
     );
-
-
 ?>
+</table>
